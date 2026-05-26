df18650edaacd08cee8f699c3821d1a17416687d
varandri <varandri@student.42antananarivo.mg>
2026-05-26 16:52:13 +0300

    features: PyMlx created

---
+++ ft_mlx.py
@@ -0,0 +1,69 @@
+# ########################################################################### #
+#                                                                             #
+#                                                          :::      ::::::::  #
+#   ft_mlx.py                                            :+:      :+:    :+:  #
+#                                                      +:+ +:+         +:+    #
+#   By: varandri <varandri@student.42antananarivo.   +#+  +:+       +#+       #
+#                                                  +#+#+#+#+#+   +#+          #
+#   Created: 2026/05/26 16:38:17 by varandri            #+#    #+#            #
+#   Updated: 2026/05/26 16:43:02 by varandri           ###   ########.fr      #
+#                                                                             #
+# ########################################################################### #
+
+from mlx import Mlx, c_void_p, c_int
+from typing import Any
+
+
+class PyMlx:
+    def __init__(self) -> None:
+        self._mlx: Mlx = Mlx()
+        self._ptr: c_void_p = self._mlx.mlx_init()
+
+    def get_ptr(self) -> c_void_p:
+        return self._ptr
+
+    def new_window(self, width: int, height: int, title: str) -> Any:
+        return self._mlx.mlx_new_window(
+            self._ptr, width, height, title
+        )
+
+    def clear_window(self, win: c_void_p) -> c_int:
+        return self._mlx.mlx_clear_window(
+            self._ptr,
+            win
+        )
+
+    def destroy_window(self, win: c_void_p) -> c_int:
+        return self._mlx.mlx_destroy_window(
+            self._ptr, win
+        )
+
+    def get_screen_size(self) -> tuple[Any, int, int]:
+        return self._mlx.mlx_get_screen_size(
+            self._ptr
+        )
+
+    def put_str(
+        self, win: Any, x: int, y: int, color: int, string: str
+    ) -> c_int:
+        return self._mlx.mlx_string_put(
+            self._ptr, win, x, y, color, string
+        )
+
+    def put_pixel(
+            self, win: c_void_p, x: int, y: int, color: int
+    ) -> c_int:
+        return self._mlx.mlx_pixel_put(
+            self._ptr, win, x, y, color
+        )
+
+    def new_image(self, width: int, height: int) -> c_void_p:
+        self._mlx.mlx_mouse_get_pos
+        return self._mlx.mlx_new_image(
+            self._ptr, width, height
+        )
+
+    def release(self) -> c_int:
+        return self._mlx.mlx_release(
+            self._ptr
+        )
